926b6e1c73db37451d5c99486d4712241c388c93
JianLi <buptlijian@163.com>
2019-07-01 11:58:09 +0800

    Add: add local blog

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    title: 'My VuePress',
+    description: '种一棵树最好的时间是十年前，其次就是现在。',
+    base:'/kakadeeBlog/',
+    repo:'https://github.com/kakadee/kakadeeBlog',
+    markdown: {
+      lineNumbers: true // 代码行数
+    },
+    themeConfig: {
+      // 添加导航栏
+      nav: [
+        { text: 'vue', link: '/' },
+        { text: 'css', link: '/blog/' },
+        { text: 'js', link: '/zhihu/' },
+        {
+          text: 'github',
+          // 这里是下拉列表展现形式。
+          items: [
+            { text: 'focus-outside', link: 'https://github.com/txs1992/focus-outside' },
+            { text: 'stylus-converter', link: 'https://github.com/txs1992/stylus-converter' }
+          ]
+        }
+      ],
+      // 为以下路由添加侧边栏
+      sidebar: ['/', '/git', '/vue']
+    },
+    
+}
---
+++ docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /logo.jpg
+actionText: 开始阅读 →
+actionLink: /accumulate/
+features:
+- title: 积累
+  details: 平时的一些积累，一些上手即用的东西/知识点等内容，范围较广。
+- title: 算法
+  details: 刷算法题，是提升编程能力和逻辑能力比较有效的一种方式。文档将算法按照难度分级，代码中都有详细注释，且会提供多种解法。
+- title: 持续更新
+  details: 本文档将会持续更新，就像我的博客一样，当做一个长期项目来维护，期望这个文档在不久以后能够给大家带来帮助。
+footer: MIT Licensed | Copyright © 2019-present kakadee
+---
